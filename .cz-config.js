f6a0d78b0506fb7e22e8012b42aef789e7efef5b
urmasmuld <urmas.muldmaa@gmail.com>
2023-10-12 19:05:04 +0300

    test

---
+++ .cz-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    path: './node_modules/cz-customizable',
+    // Add your commit message prefixes
+    scopes: [
+      'fix',
+      'feat',
+      'rel',
+    ],
+    allowCustomScopes: true,
+    allowBreakingChanges: ['feat', 'fix'],
+  };
+  
--- release.config.js
+++ release.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-    branches: ["main"], // Update with your main branch name
+    branches: ['main'],
     plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/npm",
-      "@semantic-release/github",
-      "@semantic-release/git",
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      [
+        '@semantic-release/exec',
+        {
+          prepareCmd: 'npm run postversion && git commit -am "chore(release): Update package.json version [skip ci]"',
+        },
+      ],
+      '@semantic-release/git',
     ],
   };
   
